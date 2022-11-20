fef7e811810153d39559747afcf0aed021f1c802
sagorbrur <sagorhem3532@gmail.com>
2022-11-20 22:04:54 +0600

    updated setup file with new version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools
 
 setuptools.setup(
     name="bnaug",
-    version="1.0.0",
+    version="1.1.0",
     author="Sagor Sarker",
     author_email="sagorhem3532@gmail.com",
     description="bnaug is a text augmentation tool for Bangla text.",
     long_description=codecs.open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/sagorbrur/augban",
+    url="https://github.com/sagorbrur/bnaug",
     license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -21,7 +21,6 @@ setuptools.setup(
     python_requires='>=3.7',
     install_requires=[
         "bnlp_toolkit==3.2.0",
-        "transformers==4.24.0",
-        "pytest==7.2.0",
+        "transformers==4.24.0"
     ]
 )
